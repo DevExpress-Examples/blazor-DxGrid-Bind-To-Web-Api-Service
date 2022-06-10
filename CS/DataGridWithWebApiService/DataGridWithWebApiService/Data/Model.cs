b14fd2e8f2dd32a63f9f1ec824df38ed03a96856
Elena Khamlyuk <80813840+khamlyuk@users.noreply.github.com>
2022-06-10 16:46:34 +0300

    upd (#8)

--- CS/DataGridWithWebApiService/DataGridWithWebApiService/Data/Model.cs
+++ CS/DataGridWithWebApiService/DataGridWithWebApiService/Data/Model.cs
@@ -1,12 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace DataGridWithWebApiService.Data {
+﻿namespace DataGridWithWebApiService.Data {
     class Categories {
         public int CategoryId { get; set; }
-        public string CategoryName { get; set; }
-        public string Description { get; set; }
+        public string? CategoryName { get; set; }
+        public string? Description { get; set; }
     }
 }
